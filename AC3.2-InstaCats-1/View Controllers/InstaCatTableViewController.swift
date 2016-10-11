2ce0a8856c505a631a484770048ff185c549b2b1
Louis Tur <louis.tur@gmail.com>
2016-10-11 02:52:13 -0400

    ready for lesson

--- AC3.2-InstaCats-1/View Controllers/InstaCatTableViewController.swift
+++ AC3.2-InstaCats-1/View Controllers/InstaCatTableViewController.swift
@@ -9,129 +9,34 @@
 import UIKit
 
 struct InstaCat {
-    let name: String
-    let catID: Int
-    let instagramURL: URL
-    
-    init(name: String, id: Int, instagramURL: URL) {
-        self.name = name
-        self.catID = id
-        self.instagramURL = instagramURL
-    }
-    
-    public var description: String {
-        return "Nice to me you, I'm \(self.name)"
-    }
+  // add necessary ivars and initilizations; check the tests to know what you should have
 }
 
 class InstaCatTableViewController: UITableViewController {
-    internal let InstaCatTableViewCellIdentifier: String = "InstaCatCellIdentifier"
-    internal let instaCatJSONFileName: String = "InstaCats.json"
-    internal var instaCats: [InstaCat] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let instaCatsURL: URL = self.getResourceURL(from: instaCatJSONFileName),
-            let instaCatData: Data = self.getData(from: instaCatsURL),
-            let instaCatsAll: [InstaCat] = self.getInstaCats(from: instaCatData) else {
-                return
-        }
-        
-        self.instaCats = instaCatsAll
-        
-        _ = self.instaCats.map {
-            print($0.description)
-        }
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    internal func getResourceURL(from fileName: String) -> URL? {
-        
-        // 1. There are many ways of doing this parsing, we're going to practice String traversal
-        guard let dotRange = fileName.rangeOfCharacter(from: CharacterSet.init(charactersIn: ".")) else {
-            return nil
-        }
-        
-        // 2. The upperbound of a range represents the position following the last position in the range, thus we can use it
-        // to effectively "skip" the "." for the extension range
-        let fileNameComponent: String = fileName.substring(to: dotRange.lowerBound)
-        let fileExtenstionComponent: String = fileName.substring(from: dotRange.upperBound)
-        
-        // 3. Here is where Bundle.main comes into play
-        let fileURL: URL? = Bundle.main.url(forResource: fileNameComponent, withExtension: fileExtenstionComponent)
-
-        return fileURL
-    }
-    
-    internal func getData(from url: URL) -> Data? {
-        
-        // 1. this is a simple handling of a function that can throw. In this case, the code makes for a very short function
-        // but it can be much larger if we change how we want to handle errors.
-        let fileData: Data? = try? Data(contentsOf: url)
-        return fileData
-    }
-    
-    internal func getInstaCats(from jsonData: Data) -> [InstaCat]? {
-        
-        // 1. This time around we'll add a do-catch
-        do {
-            let instaCatJSONData: Any = try JSONSerialization.jsonObject(with: jsonData, options: [])
-            
-            // 2. Cast from Any and check for the "cats" key
-            guard let instaCatJSONCasted: [String : AnyObject] = instaCatJSONData as? [String : AnyObject],
-                let instaCatArray: [AnyObject] = instaCatJSONCasted["cats"] as? [AnyObject] else {
-                return nil
-            }
-            
-            // 3.
-            var instaCats: [InstaCat] = []
-            instaCatArray.forEach({ instaCatObject in
-                guard let instaCatName: String = instaCatObject["name"] as? String,
-                    let instaCatIDString: String = instaCatObject["cat_id"] as? String,
-                    let instaCatInstagramURLString: String = instaCatObject["instagram"] as? String,
-                    
-                    // 4. Some of these values need further casting
-                    let instaCatID: Int = Int(instaCatIDString),
-                    let instaCatInstagramURL: URL = URL(string: instaCatInstagramURLString) else {
-                        return
-                }
-                
-                // 5. append to our temp array
-                instaCats.append(InstaCat(name: instaCatName, id: instaCatID, instagramURL: instaCatInstagramURL))
-            })
-            
-            return instaCats
-        }
-        catch let error as NSError {
-            // JSONSerialization doc specficially says an NSError is returned if JSONSerialization.jsonObject(with:options:) fails
-           print("Error occurred while parsing data: \(error.localizedDescription)")
-        }
-    
-        return  nil
-    }
 
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.instaCats.count
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: InstaCatTableViewCellIdentifier, for: indexPath)
-        
-        cell.textLabel?.text = self.instaCats[indexPath.row].name
-        cell.detailTextLabel?.text = self.instaCats[indexPath.row].description
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+
         return cell
     }
 
--- AC3.2-InstaCats-1Tests/AC3_2_InstaCats_1Tests.swift
+++ AC3.2-InstaCats-1Tests/AC3_2_InstaCats_1Tests.swift
@@ -32,54 +32,53 @@ class AC3_2_InstaCats_1Tests: XCTestCase {
     }
     
     func testInitializerOfInstaCat() {
-        let testInstaCat: InstaCat = InstaCat(name: testName, id: testID, instagramURL: testURL)
-        
-        XCTAssertTrue(testInstaCat.name == testName)
-        XCTAssertTrue(testInstaCat.catID == testID)
-        XCTAssertTrue(testInstaCat.instagramURL == testURL)
+//        let testInstaCat: InstaCat = InstaCat(name: testName, id: testID, instagramURL: testURL)
+//        
+//        XCTAssertTrue(testInstaCat.name == testName)
+//        XCTAssertTrue(testInstaCat.catID == testID)
+//        XCTAssertTrue(testInstaCat.instagramURL == testURL)
     }
     
     func testValidInstaCatDescription() {
-        let testInstaCat: InstaCat = InstaCat(name: testName, id: testID, instagramURL: testURL)
-        let expectedDescription: String = "Nice to me you, I'm \(testName)"
-        
-        XCTAssertTrue(testInstaCat.description == expectedDescription)
+//        let testInstaCat: InstaCat = InstaCat(name: testName, id: testID, instagramURL: testURL)
+//        let expectedDescription: String = "Nice to me you, I'm \(testName)"
+//        
+//        XCTAssertTrue(testInstaCat.description == expectedDescription)
     }
     
     func testGetResourceURLFromFilename() {
-        let testFileName: String = "test.json"
-        let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName)
-        XCTAssertNotNil(testFileURL, "getResourceURL(from:) should return a non-nil URL for a valid file")
-        
-        let invalidFileName: String = "testing.json"
-        let invalidTestFileURL = testInstaCatTableVC.getResourceURL(from: invalidFileName)
-        XCTAssertNil(invalidTestFileURL, "getResourceURL(from:) should return nil for a file that does not exist")
-        
-        let malformedFileName: String = "testedjson"
-        let malformedFileURL = testInstaCatTableVC.getResourceURL(from: malformedFileName)
-        XCTAssertNil(malformedFileURL, "getResourceURL(from:) should return nil for an improperly formatted filename parameter")
-        
+//        let testFileName: String = "test.json"
+//        let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName)
+//        XCTAssertNotNil(testFileURL, "getResourceURL(from:) should return a non-nil URL for a valid file")
+//        
+//        let invalidFileName: String = "testing.json"
+//        let invalidTestFileURL = testInstaCatTableVC.getResourceURL(from: invalidFileName)
+//        XCTAssertNil(invalidTestFileURL, "getResourceURL(from:) should return nil for a file that does not exist")
+//        
+//        let malformedFileName: String = "testedjson"
+//        let malformedFileURL = testInstaCatTableVC.getResourceURL(from: malformedFileName)
+//        XCTAssertNil(malformedFileURL, "getResourceURL(from:) should return nil for an improperly formatted filename parameter")
     }
     
     func testGetDataFromURL() {
-        let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName)
-        let testData = testInstaCatTableVC.getData(from: testFileURL!)
-        XCTAssertNotNil(testData, "getData(from:) should return a non-nil URL for a valid file")
+//        let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName)
+//        let testData = testInstaCatTableVC.getData(from: testFileURL!)
+//        XCTAssertNotNil(testData, "getData(from:) should return a non-nil URL for a valid file")
     }
     
     func testGettingInstaCatsFromData() {
-        guard let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName),
-            let testFileData = testInstaCatTableVC.getData(from: testFileURL),
-            let testFileInstaCats = testInstaCatTableVC.getInstaCats(from: testFileData)
-        else {
-            XCTFail()
-            return
-        }
-        
-        XCTAssertTrue(testFileInstaCats.count == 3)
-        XCTAssertTrue(testFileInstaCats[0].name == "Nala Cat")
-        XCTAssertTrue(testFileInstaCats[1].catID == 002)
-        XCTAssertTrue(testFileInstaCats[2].instagramURL == URL(string: "https://www.instagram.com/grump_cat_/?hl=en"))
+//        guard let testFileURL = testInstaCatTableVC.getResourceURL(from: testFileName),
+//            let testFileData = testInstaCatTableVC.getData(from: testFileURL),
+//            let testFileInstaCats = testInstaCatTableVC.getInstaCats(from: testFileData)
+//        else {
+//            XCTFail()
+//            return
+//        }
+//        
+//        XCTAssertTrue(testFileInstaCats.count == 3)
+//        XCTAssertTrue(testFileInstaCats[0].name == "Nala Cat")
+//        XCTAssertTrue(testFileInstaCats[1].catID == 002)
+//        XCTAssertTrue(testFileInstaCats[2].instagramURL == URL(string: "https://www.instagram.com/grump_cat_/?hl=en"))
     }
 
 }
